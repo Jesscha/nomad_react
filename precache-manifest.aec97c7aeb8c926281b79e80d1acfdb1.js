4b14ff801580b9392875600078686b14e2217343
jessecha <jessecha222@gmail.com>
2020-03-30 18:13:54 +0900

    Updates

---
+++ precache-manifest.aec97c7aeb8c926281b79e80d1acfdb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d259ab7f3459e8deb81b76fe09a7bbc5",
+    "url": "/nomad_react/index.html"
+  },
+  {
+    "revision": "9c9832be5a958bbf44a7",
+    "url": "/nomad_react/static/css/main.f7efc5af.chunk.css"
+  },
+  {
+    "revision": "f4a85d0fefc06d4e9459",
+    "url": "/nomad_react/static/js/2.ff8ef10b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad_react/static/js/2.ff8ef10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9832be5a958bbf44a7",
+    "url": "/nomad_react/static/js/main.a5a98f14.chunk.js"
+  },
+  {
+    "revision": "e17d6b2f2564b3ee03a4",
+    "url": "/nomad_react/static/js/runtime-main.375a53c6.js"
+  }
+]);
